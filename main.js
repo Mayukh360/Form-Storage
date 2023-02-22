98bd2c1ace0291abba941ad2c0e5985081d38e12
Mayukh <mayukhchatterjee2023@gmail.com>
2023-02-22 22:00:27 +0530

    Creating form

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+document.getElementById("myButton").onclick= function(){
+  
+    let myName=document.getElementById("name").value;
+    let myEmail=document.getElementById("email").value;
+   
+    localStorage.setItem(myName , myEmail)
+   
+    
+}
+
+
+
